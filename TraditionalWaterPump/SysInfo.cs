d3c1431eb12c08d3294a712e7577f0199a4a6a83
“ad64001” <3192364930@qq.com>
2026-02-16 17:59:03 +0800

    添加9#

---
+++ TraditionalWaterPump/SysInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xbd.s7netplus;
+
+namespace TraditionalWaterPump
+{
+    public class SysInfo
+    {
+        public CpuType CpuType { get; set; }
+
+        public string IpAddress { get; set; }
+
+        /// <summary>
+        /// 机架号
+        /// </summary>
+        public short Rack { get; set; }
+
+        /// <summary>
+        /// 插槽号
+        /// </summary>
+        public short Slot { get; set; }
+
+
+        /// <summary>
+        /// 开机自启
+        /// </summary>
+        public bool AutoStart { get; set; }
+
+        /// <summary>
+        /// 无操作熄屏时间
+        /// </summary>
+        public int ScreenTime { get; set; }
+
+        /// <summary>
+        /// 自动注销时间
+        /// </summary>
+        public int LogoffTime { get; set; }
+
+        /// <summary>
+        /// 摄像头索引
+        /// </summary>
+        public int CameraIndex { get; set; }
+    }
+}
---
+++ TraditionalWaterPump/SysInfoService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xbd.s7netplus;
+
+namespace TraditionalWaterPump
+{
+    internal class SysInfoService
+    {
+        /// <summary>
+        /// 读配置返回系统信息
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public SysInfo GetSysInfoFromPath(string path)
+        {
+            try
+            {
+                SysInfo sysInfo = new SysInfo();
+                sysInfo.IpAddress = IniConfigHelper.ReadIniData("通信参数", "IP地址", "127.0.0.1", path);
+                sysInfo.CpuType = (CpuType)Enum.Parse(typeof(CpuType),IniConfigHelper.ReadIniData("通信参数","CPU类型","S7200Smart",path),true);
+                sysInfo.Rack =Convert.ToInt16(IniConfigHelper.ReadIniData("通信参数", "机架号", "0", path));
+                sysInfo.Slot =Convert.ToInt16(IniConfigHelper.ReadIniData("通信参数", "插槽号", "0", path));
+
+                sysInfo.AutoStart = IniConfigHelper.ReadIniData("系统参数", "开机启动", "1", path) == "1";
+                sysInfo.ScreenTime = Convert.ToInt32(IniConfigHelper.ReadIniData("系统参数", "熄屏时间", "0", path));
+                sysInfo.LogoffTime = Convert.ToInt32(IniConfigHelper.ReadIniData("系统参数", "注销时间", "0", path));
+                sysInfo.CameraIndex = Convert.ToInt32(IniConfigHelper.ReadIniData("系统参数", "摄像头序号", "0", path));
+
+                return sysInfo;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 将SysInfo写入配置文件
+        /// </summary>
+        /// <param name="sysInfo"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public bool SetSysInfoToPath(SysInfo sysInfo,string path)
+        {
+            bool result = true;
+
+            result &= IniConfigHelper.WriteIniData("通信参数", "IP地址", sysInfo.IpAddress, path);
+            result &= IniConfigHelper.WriteIniData("通信参数", "CPU类型", sysInfo.CpuType.ToString(), path);
+            result &= IniConfigHelper.WriteIniData("通信参数", "机架号", sysInfo.Rack.ToString(), path);
+            result &= IniConfigHelper.WriteIniData("通信参数", "插槽号", sysInfo.Slot.ToString(), path);
+
+            result &= IniConfigHelper.WriteIniData("系统参数", "开机启动", sysInfo.AutoStart?"1":"0", path);
+            result &= IniConfigHelper.WriteIniData("系统参数", "熄屏时间", sysInfo.ScreenTime.ToString(), path);
+            result &= IniConfigHelper.WriteIniData("系统参数", "注销时间", sysInfo.LogoffTime.ToString(), path);
+            result &= IniConfigHelper.WriteIniData("系统参数", "摄像头序号", sysInfo.CameraIndex.ToString(), path);
+
+            return result;
+        }
+    }
+}
